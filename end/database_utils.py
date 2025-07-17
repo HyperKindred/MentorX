05bbdbc454073a5a2847a21170e9f65be71a3e93
Ninestates9 <1955469225@qq.com>
2025-07-17 22:09:05 +0800

    fix get course list db

--- end/database_utils.py
+++ end/database_utils.py
@@ -263,13 +263,13 @@ def get_ai_chat_db(student_id, chapter_id):
 def get_course_list_db(student_id = None, teacher_id = None):
     conn, cursor = connectSQL()
     try:
-        if student_id is None:
+        if student_id is None and teacher_id is None:
             sql = "SELECT DISTINCT id FROM course;"
             cursor.execute(sql)
         elif teacher_id is not None:
             sql = "SELECT DISTINCT id FROM course WHERE teacher = %s);"
             cursor.execute(sql, (teacher_id))
-        else:
+        elif student_id is not None:
             sql = "SELECT DISTINCT course_id FROM course_student WHERE student_id = %s;"
             cursor.execute(sql, (student_id))
         course_ids = [row[0] for row in cursor.fetchall()]
@@ -288,7 +288,7 @@ def get_course_list_db(student_id = None, teacher_id = None):
             cursor.execute(sql, (course_id,))
             student_count = cursor.fetchone()[0]
             
-            if teacher_id is not None:
+            if teacher_id is None:
                 courses.append({
                     "id": course_id,
                     "name": course_info[0],
