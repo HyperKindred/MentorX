c1871a13882709554648e15ac08a3feadf72a62b
Ninestates9 <1955469225@qq.com>
2025-07-17 22:11:49 +0800

    delete extra )

--- end/database_utils.py
+++ end/database_utils.py
@@ -267,7 +267,7 @@ def get_course_list_db(student_id = None, teacher_id = None):
             sql = "SELECT DISTINCT id FROM course;"
             cursor.execute(sql)
         elif teacher_id is not None:
-            sql = "SELECT DISTINCT id FROM course WHERE teacher = %s);"
+            sql = "SELECT DISTINCT id FROM course WHERE teacher = %s;"
             cursor.execute(sql, (teacher_id))
         elif student_id is not None:
             sql = "SELECT DISTINCT course_id FROM course_student WHERE student_id = %s;"
